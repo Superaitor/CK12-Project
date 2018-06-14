c0cb0f99843b93aff4f44d07ed15d5ebf2e36bdf
Superaitor <amatriai@usc.edu>
2018-06-14 16:56:12 -0700

    Update Multiple choice questions.py

--- Multiple choice questions.py
+++ Multiple choice questions.py
@@ -1,9 +1,7 @@
 from word_2_vec import main
-from nltk.corpus import words as wds
 from nltk.corpus import wordnet as wn
 from random import randint
 from Most_Similar import main3
-from gensim.models import word2vec
 
 
 def main2():
@@ -17,39 +15,38 @@ def main2():
 
 
 def fill_in_the_blank(x):
-    correct_answer = " "
     n = 4  # number of questions you want + 1
-    m = 4  # number of multiple choice questions + 1
-    tester = m
     words = list()
-    quests = [None] * n
-    answers = [None] * (tester + 1)
     while n > 0:
         random = randint(0, 10)
         if x[random] not in words:
             words.append(x[random])
             n -= 1
     for q in words:
-        y = wn.synset(q + '.n.01').definition()
         t = False
-        while(t is False):
-            answer = input("Fill in the blank: " + y + " :")
-            if answer == q:
-                print("Correct!")
-                t = True
-            else:
-                print("Try again!")
+        y = wn.synsets(q)
+        if y:
+            sol = y[0].definition()
+            while t is False:
+                answer = input("Fill in the blank: " + sol + " :")
+                if answer == q:
+                    print("Correct!")
+                    t = True
+                else:
+                    print("Try again!")
 
 
 def multiple_choices(x):
-    n = 4  # number of questions you want + 1
+    wrong = 0
+    incorrect = list()
+    n = len(x)  # number of questions you want + 1
     m = 4  # number of multiple choice questions + 1
     tester = m
     words = list()
-    quests = [None] * n
+    end = False
 
-    while n > 0:
-        random = randint(0, 10)
+    while n > 0 and end is False:
+        random = randint(0, len(x)- 1)
         if x[random] not in words:
             words.append(x[random])
             n -= 1
@@ -66,42 +63,56 @@ def multiple_choices(x):
                 lgth -= 1
         x = 0
         output = list()
-        while (x < len(answers)):
+        while x < len(answers):
             if x == 0:
-                output.append("a: " + answers[x])
+                output.append("a) " + answers[x])
                 if answers[x] == w:
                     correct_answer = "a"
                 x += 1
             if x == 1:
-                output.append("b: " + answers[x])
+                output.append("\nb) " + answers[x])
                 if answers[x] == w:
                     correct_answer = "b"
                 x += 1
             if x == 2:
-                output.append("c: " + answers[x])
+                output.append("\nc) " + answers[x])
                 if answers[x] == w:
                     correct_answer = "c"
                 x += 1
             if x == 3:
-                output.append("d: " + answers[x])
+                output.append("\nd) " + answers[x])
                 if answers[x] == w:
                     correct_answer = "d"
                 x += 1
             if x == 4:
-                output.append("e: " + answers[x])
+                output.append("\ne) " + answers[x])
                 if answers[x] == w:
                     correct_answer = "e"
                 x += 1
         t = False
         while t is False:
-            y = wn.synset(w + '.n.01').definition()
-            print(output)
-            ans = input("Enter the correct letter answer: " + y + " : ")
-            if ans == correct_answer:
-                print("Nice!")
-                t = True
+            controller = 0
+            y = wn.synsets(w)
+            if y:
+                sol = y[0].definition()
+                ans = input("Enter the correct letter answer (or 's' to stop): " + sol + " : \n" + ''.join(output))
+                if ans == correct_answer:
+                    print("Nice!")
+                    t = True
+                if ans == "s":
+                    end = True
+                    break
+                else:
+                    print("Try again!")
+                    wrong += 1
+                    if controller is 0:
+                        incorrect.append(w)
+                        controller += 1
             else:
-                print("Try again!")
-
-
-main2()
+                break
+        if end is True:
+            break
+    print("Good Job, you finished this automated test. You incorrectly answered questions " + str(wrong) + " times.")
+    print("Given your performance in this test, you should practice learning more about:")
+    for words in incorrect:
+        print("- " + words)
